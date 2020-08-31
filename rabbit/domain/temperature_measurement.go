db9e0ba1da370d8a4c43e95c38051c1f1521cf6f
rfashwal <rfashwal@hotmail.com>
2020-08-31 17:16:37 +0300

    add room id

--- rabbit/domain/temperature_measurement.go
+++ rabbit/domain/temperature_measurement.go
@@ -3,9 +3,10 @@ package domain
 import "time"
 
 type TemperatureMeasurement struct {
-	ProcessId   string    `json:"processId"`
-	SensorId    string    `json:"sensorId"`
+	ProcessId   string    `json:"process_id"`
+	RoomId      string    `json:"room_id"`
+	SensorId    string    `json:"sensor_id"`
 	Service     string    `json:"service"`
 	Value       float64   `json:"value"`
-	PublishedOn time.Time `json:"publishedOn"`
+	PublishedOn time.Time `json:"published_on"`
 }
